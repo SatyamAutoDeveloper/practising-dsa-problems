a8058018d55cc1979fda70efd1750a8a02148dfa
Satyam Agrawal <satyamqa1996@gmail.com>
2025-06-11 17:04:42 +0530

    Circular Doubly LL and Simplify Spiral Matrix LeetCode Problem

--- Array/spiral_matrix_problem.py
+++ Array/spiral_matrix_problem.py
@@ -5,35 +5,37 @@ class SpiralMatrix:
             print("Spiral Order for given matrix :: []")
             return []
         spiral_order = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
+        
+        srow = 0
+        scol = 0
+        erow = len(matrix) - 1
+        ecol = len(matrix[0]) - 1
 
-        while top <= bottom and left <= right:
-            # Traverse from Left to Right
-            for i in range(left, right + 1):
-                spiral_order.append(matrix[top][i])
-            top += 1
+        while srow <= erow and scol <= ecol:
+            for i in range(scol, ecol+1):
+                """Traverse from left to right"""
+                spiral_order.append(matrix[srow][i])
+            srow = srow + 1
+            
+            for j in range(srow, erow + 1):
+                """Traverse from top to bottom"""
+                spiral_order.append(matrix[j][ecol])
+            ecol = ecol - 1
+            
+            if srow <= erow:
+                for i in range(ecol, scol - 1, -1):
+                    """Traverse from right to left"""
+                    spiral_order.append(matrix[erow][i])
+                erow = erow - 1
+            
+            if scol <= ecol:
+                for j in range(erow, srow - 1, -1):
+                    """Traverse from bottom to top"""
+                    spiral_order.append(matrix[j][scol])
+                scol = scol + 1
 
-            # Traverse Downwards
-            for i in range(top, bottom + 1):
-                spiral_order.append(matrix[bottom if left > right else i][right])
-            right -= 1
-
-            if top <= bottom:
-                # Traverse from Right to Left
-                for i in range(right, left - 1, -1):
-                    spiral_order.append(matrix[bottom][i])
-                bottom -= 1
-
-            if left <= right:
-                # Traverse Upwards
-                for i in range(bottom, top - 1, -1):
-                    spiral_order.append(matrix[i][left])
-                left += 1
-
-        print("Spiral Order for given matrix ::", spiral_order)
         return spiral_order
 
-
 sm = SpiralMatrix()
-sm.spiralOrder([[2,5,8],[4,0,-1]])
+spiral_order_list = sm.spiralOrder([[2,5],[8,4],[0,-1]])
+print("Spiral Order:: ", spiral_order_list)
