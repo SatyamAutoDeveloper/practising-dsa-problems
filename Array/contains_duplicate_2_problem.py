7805a15305a78395d3fc871bae140fb3d5eef864
Satyam Agrawal <satyamqa1996@gmail.com>
2025-06-24 16:58:45 +0530

    Contains Duplicate 2 Leetcode Problem

---
+++ Array/contains_duplicate_2_problem.py
@@ -0,0 +1,25 @@
+"""
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+class ContainsDuplicate2:
+    def containsDuplicate2(self, nums, k):
+        dict1 = {}
+        for i in range(len(nums)):
+            if nums[i] in dict1 and abs(i-dict1[nums[i]]) <= k:
+                return True
+            dict1[nums[i]] = i
+        return False
+
+cd2 = ContainsDuplicate2()
+print("Duplicates distinct indices substraction is equal to or less than K ::",cd2.containsDuplicate2([1,0,1,1], 1))
+
+
+"""for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if j > i + k:
+                    break 
+                if ((nums[i] == nums[j]) and ((j - i) <= k)):
+                    return True
+        return False"""
